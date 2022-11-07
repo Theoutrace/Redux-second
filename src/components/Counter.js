bae3a79b92c73cb4338ea326b2d2634677a05b11
Theoutrace <prakashkumar.imw@gmail.com>
2022-11-07 23:46:03 +0530

    increment : decrement button done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,46 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+
+  const counter = useSelector((state)=> state.counter)
+  const dispatch = useDispatch()
+
+
+  const incrementHandler = ()=>{
+    dispatch({ type: 'increment'})
+  }
+
+  const decrementHandler = ()=>{
+    dispatch({ type: 'decrement'})
+  }
+
+  const incrementByFiveHandler = ()=>{
+    dispatch({type: 'incrementBy5'})
+  }
+  const decrementByFiveHandler = ()=>{
+    dispatch({type: 'decrementBy5'})
+  }
+
+
+  // const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={incrementByFiveHandler}>Increment by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={decrementByFiveHandler}>Decrement by 5</button>
+      </div>
+      <button onClick={'toggleCounterHandler'}>Toggle Counter</button>
     </main>
   );
 };
 
 export default Counter;
+// useSelector: automatically select a part of the state managed by the store
+// automatically sets up the subscription to the redux store for this component and component receives latest counter automatically on change.
+// returns the returned value from the func passed in selector.
--- src/store/index.js
+++ src/store/index.js
@@ -1,38 +1,36 @@
-import {createStore} from 'redux'
+import { createStore } from "redux";
 
 // step 1 create a reducer--------------------------------------------------
-const counterReducer = (state={counter:0}, action)=>{
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
 
-    if(action.type === 'increment'){
-        return {
-            counter: state.counter+1
-        }
-    }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
 
-    if(action.type === 'decrement'){
-        return {
-            counter: state.counter -1
-        }
-    }
+  if (action.type === "incrementBy5") {
+    return {
+      counter: state.counter + 5,
+    };
+  }
 
-    if(action.type === 'INCREMENTBY2'){
-        return{
-            counter: state.counter +2
-        }
-    }
+  if (action.type === "decrementBy5") {
+    return {
+      counter: state.counter - 5,
+    };
+  }
 
-    if(action.type === 'DECREMENTBY2'){
-        return{
-            counter: state.counter -2
-        }
-    }
-
-    return state
-}
+  return state;
+};
 
 //step 2 create a store  --->>>>  step 3 pass the reducer in the create store function
-const store = createStore(counterReducer)
-
+const store = createStore(counterReducer);
 
 // step 3 is to connect this store to components so that they can dispatch actions and listen to store state values.
-export default store
+export default store;
